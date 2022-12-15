0d330c91159db4e19d4e517350ffb8272d4d6ffa
Agustín Bianchi <bianchiagus6@gmail.com>
2022-12-15 20:19:35 -0300

    disable user-selection

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{ userSelect: 'none' }}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
